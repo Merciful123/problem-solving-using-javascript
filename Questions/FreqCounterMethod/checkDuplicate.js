9f4e96800bfd2d4d5360f8f69a4e64bc61d37456
Aamir Raza <merciful9795@gmail.com>
2023-08-28 20:53:24 +0530

    frequency method exercise solved

---
+++ Questions/FreqCounterMethod/checkDuplicate.js
@@ -0,0 +1,34 @@
+function areThereDuplicates() {
+  let count = {};
+  for (let i = 0; i < arguments.length; i++) {
+    count[arguments[i]] ? count[arguments[i]]++ : (count[arguments[i]] = 1);
+  }
+  console.log(count);
+
+  for (let i in count) {
+    if (count[i] > 1) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// console.log(areThereDuplicates(1, 2, 3, 4, 10, 10));
+
+
+function areThereDuplicates() {
+    const frequencyCounter = {};
+    for (let arg of arguments) {
+        // first time it is false as no elements in FrequencyCounter
+        if (frequencyCounter[arg]) {
+            return true;
+        }
+        // this adds element in frequency counter
+        frequencyCounter[arg] = 1;
+    }
+    return false;
+}
+
+// Test cases
+// console.log(areThereDuplicates(1, 2, 3));          // false

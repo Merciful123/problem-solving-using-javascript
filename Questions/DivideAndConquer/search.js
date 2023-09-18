4cb801206f47d9fc3ed7f3ba0a95a0c28ba04368
Aamir Raza <merciful9795@gmail.com>
2023-09-18 12:59:26 +0530

    divide and conquer method problem revised

--- Questions/DivideAndConquer/search.js
+++ Questions/DivideAndConquer/search.js
@@ -17,4 +17,26 @@ const search = (arr, value) => {
   }
   return -1;
 };
-console.log(search([1, 2, 3, 4, 5, 10, 15], 12));
+console.log(search([1, 2, 3, 4, 5, 10, 15], 15));
+
+const search1 = (arr, num) => {
+  if (!num) return "No number to find"
+  let start = 0
+
+  let end = arr.length - 1
+  
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2)
+    let average  = arr[mid]
+    console.log(start, end)
+    if (average < num) start = mid + 1
+    else if (average > num) end = mid - 1
+    
+    else return mid
+
+
+  }
+  return -1
+
+}
+console.log(search1([1, 2, 3, 4, 5, 10, 15], 15));

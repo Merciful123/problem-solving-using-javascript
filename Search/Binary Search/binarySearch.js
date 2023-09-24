2032a62db74554da25e85dac01cee201cbdd7bcc
Aamir Raza <merciful9795@gmail.com>
2023-09-24 17:19:20 +0530

    binary search fundamental revised

--- Search/Binary Search/binarySearch.js
+++ Search/Binary Search/binarySearch.js
@@ -26,8 +26,63 @@ function binarySearch(arr, elem) {
     if (elem < arr[middle]) end = middle - 1;
     else start = middle + 1;
     middle = Math.floor((start + end) / 2);
+
+
   }
   return arr[middle] === elem ? middle : -1;
 }
 
-binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103);
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 13))
+
+// revision
+
+const binarySearchRevise = (arr, elem) => {
+  
+  let start = 0
+
+  let end = arr.length - 1
+  
+  let middle = Math.floor((start + end)/2)
+
+  while (arr[middle] !== elem && start <= end) {
+    
+    if (elem > arr[middle]) start = middle + 1
+    
+    else end = middle - 1
+
+    middle = Math.floor((start + end) / 2)
+
+  }
+  return arr[middle] === elem ? middle : -1
+
+}
+
+console.log(binarySearchRevise([2, 5, 6, 9, 13, 15, 28, 30], 13));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

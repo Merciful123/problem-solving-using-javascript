c828f4d36cac596cecf238e17429d67bde2bc209
Aamir Raza <raza949597@gmail.com>
2025-11-15 09:39:31 +0530

    cachedAPI call funtion improves

--- Questions/Miscellaneous/practice.js
+++ Questions/Miscellaneous/practice.js
@@ -38,129 +38,7 @@ function join(a,b,c){
 // console.log(curriedJoin(1)(2)(3))
 
 
-// function cachedApiCall (cacheTime) {
 
-//     let cacheResult = new Map();
-//     console.log(cacheTime)
-
-//     return async (url, options) =>{
-
-//         console.log("sec0", url)
-        
-//         let cacheKey = JSON.stringify({url, options});
-
-//         let currentTime = new Date().getSeconds();
-
-//         console.log(currentTime ?? "NA");
-        
-//         // check cache first before network request
-
-//         if(cacheResult.has(cacheKey)){
-//             const {data, timeStamp} = cacheResult.get(cacheKey);
-
-//             console.log(timeStamp ?? "NA");
-//             console.log(currentTime ?? "NA");
-
-//             if(currentTime - timeStamp < cacheTime){
-//                 return data;
-//             }else{
-//                 cacheResult.delete(cacheKey);
-//             }
-//         }
-
-//         // writing code for API call first
-        
-//         try{
-            
-//             console.log("making api call")
-//             console.log(currentTime)
-
-//             const response = await fetch(url, options);
-
-//             console.log("sec", url)
-            
-//             const data = response.json();
-            
-//             cacheResult.set(cacheKey, {data: data, timeStamp: currentTime})
-
-//             return data;
-
-//         } catch(error){
-//             console.log(error)
-//         }   
-//     }
-// }
-
-
-
-function cachedApiCall (cacheTime){
-
-    let cacheResult = new Map();
-
-
-
-    // first make an API call
-
-    return async (url, options) => {
-
-        let cacheKey = JSON.stringify({url, options})
-
-        let currentTime = Date.now();
-        console.log(cacheTime, currentTime)
-        console.log("diff - ",currentTime - currentTime)
-
-        if (cacheResult.has(cacheKey)){
-
-            const { data, timeStamp } = cacheResult.get(cacheKey);
-
-            if ((currentTime - timeStamp )< cacheTime) {
-                return data;
-            }else{
-                cacheResult.delete(cacheKey);
-
-            }
-        }
-
-        try{
-            const response = await fetch(url, options); 
-            const result = response.json();
-
-            cacheResult.set(cacheKey, {data : result, timeStamp: currentTime});
-
-            return result;
-
-        } catch(error){
-            console.log("error: - ", error)
-        }
-    }
-
-}
-
-
-
-// const call = cachedApiCall(1500);
-
-call("https://jsonplaceholder.typicode.com/todos/1", {}).then((a) =>
-  console.log("1",a)
-);
-
-// This will make a network call and log "1" with the data.
-
-setTimeout(() => {
-  call("https://jsonplaceholder.typicode.com/todos/1", {}).then((a) =>
-    console.log("2", a)
-  );
-}, 800);
-
-// This will use the cache and log "2" with the same data.
-
-setTimeout(() => {
-  call("https://jsonplaceholder.typicode.com/todos/1", {}).then((a) =>
-    console.log("3", a)
-  );
-}, 1700);
-
-// This will make a new network call because the cache is expired, and log "3" with the data (which might be the same or different).
 
 
 function cachedApiCall(cacheTime) {
@@ -172,11 +50,13 @@ function cachedApiCall(cacheTime) {
         const callId = callCount;
         let cacheKey = JSON.stringify({url, options});
         let currentTime = Date.now();
+        console.log("currentTime", currentTime)
         
 
         if (cacheResult.has(cacheKey)) {
             const cachedData = cacheResult.get(cacheKey);
             const timeDiff = currentTime - cachedData.timeStamp;
+            console.log("cachee",cachedData.timeStamp)
             
             console.log(` Time difference: ${timeDiff}ms`);
             
